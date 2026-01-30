b2f45f8dd2c008627e59f9187eb9b88c178f68c6
olektrolek <gosiawiktor2410@gmail.com>
2026-01-30 12:07:39 +0100

    Multiple changes - settings page - background script - some new features

--- hide-rating.js
+++ hide-rating.js
@@ -1,132 +1,191 @@
 if (window.location.href.startsWith("https://letterboxd.com/film/")) {
-    const hideSelectors = [
-        "#film-page-wrapper > div.col-17 > aside > section.section.ratings-histogram-chart",
-        "#js-poster-col > section.poster-list.-p230.-single.no-hover.el.col > div.production-statistic-list > div.production-statistic.-top250",
-        "#js-poster-col > section.poster-list.-p230.-single.no-hover.el.col > div.production-statistic-list > div.production-statistic.-likes",
-        "#film-page-wrapper > div.col-17 > section.film-recent-reviews.-clear > section.film-reviews.section.js-popular-reviews",
-        "#film-page-wrapper > div.col-17 > section.film-recent-reviews.-clear > section.film-reviews.section.js-recent-reviews",
-        "#film-page-wrapper > div.col-17 > section.section.activity-from-friends.-clear.-friends-watched.-no-friends-want-to-watch",
-        "#film-page-wrapper > div.col-17 > section.film-recent-reviews.-clear > section.film-reviews.section.js-popular-friend-reviews",
-        "#production-popular-lists",
-        "#lists"
-    ]
+    chrome.storage.local.get(null, (data) => {
+        if (data.extensionState) {
+            // hide while loading
+            let loading = true
+            const loadingSelectors = [
+                "#film-page-wrapper > div.col-17 > aside > section.section.ratings-histogram-chart",
+                "#js-poster-col > section.poster-list.-p230.-single.no-hover.el.col > div.production-statistic-list",
+                "#film-page-wrapper > div.col-17 > section.film-recent-reviews.-clear > section.film-reviews.section.js-popular-reviews",
+                "#film-page-wrapper > div.col-17 > section.film-recent-reviews.-clear > section.film-reviews.section.js-recent-reviews",
+                "#film-page-wrapper > div.col-17 > section.section.activity-from-friends.-clear"
+            ]
+            const observer = new MutationObserver(() => {
+                if (!loading) return
+                loadingSelectors.forEach(sel => {
+                    const el = document.querySelector(sel)
+                    if (el) el.style.visibility = "hidden"
+                })
+            })
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            })
+            // main hiding logic
+            const hideSelectors = []
+            const hideElements = []
 
-    // hide while loading
-    let loading = true
-    const loadingSelectors = [
-        "#film-page-wrapper > div.col-17 > aside > section.section.ratings-histogram-chart",
-        "#js-poster-col > section.poster-list.-p230.-single.no-hover.el.col > div.production-statistic-list",
-        "#film-page-wrapper > div.col-17 > section.film-recent-reviews.-clear > section.film-reviews.section.js-popular-reviews",
-        "#film-page-wrapper > div.col-17 > section.film-recent-reviews.-clear > section.film-reviews.section.js-recent-reviews",
-        "#film-page-wrapper > div.col-17 > section.section.activity-from-friends.-clear"
-    ]
-    const observer = new MutationObserver(() => {
-        if (!loading) return
-        loadingSelectors.forEach(sel => {
-            const el = document.querySelector(sel)
-            if (el) el.style.visibility = "hidden"
-        })
-    })
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true
-    })
-
-    // hide/show rating
-    function hideRatings() {
-        let li = document.createElement("li")
-        document.querySelector("#userpanel > ul").appendChild(li)
-        let toggleBtn = document.createElement("button")
-        li.appendChild(toggleBtn)
-
-        function changeState(hidden) {
-            let displayState
-            if (hidden == true) {
-                displayState = "none"
-                toggleBtn.textContent = "Show rating"
-            } else if (hidden == false) {
-                displayState = ""
-                toggleBtn.textContent = "Hide rating"
+            if (data.hideRating) {
+                hideSelectors.push("#film-page-wrapper > div.col-17 > aside > section.section.ratings-histogram-chart")
             }
-            hideSelectors.forEach(sel => { 
-                const el = document.querySelector(sel)
-                if (sel == "#film-page-wrapper > div.col-17 > section.section.activity-from-friends.-clear.-friends-watched.-no-friends-want-to-watch") {
-                    let friends = document.querySelectorAll("#film-page-wrapper > div.col-17 > section.section.activity-from-friends.-clear > ul > li > a > span.rating")
-                    friends.forEach(friend => {
-                        if (displayState) {
-                            friend.style.display = "none"
-                            let hiddenRating = document.createElement("span")
-                            hiddenRating.textContent = "???"
-                            hiddenRating.className = "hiddenRating"
-                            hiddenRating.style.top = "4px"
-                            hiddenRating.style.fontWeight = "600"
-                            hiddenRating.style.position = "relative"
-                            friend.parentElement.style.textAlign = "center"
-                            friend.parentElement.appendChild(hiddenRating)
-                        } else if (!displayState) {
-                            friend.style.display = ""
-                            let deleteSpan = document.querySelector(".hiddenRating")
-                            if (deleteSpan) {
-                                deleteSpan.remove()
+            if (data.hideWatches) {
+                hideSelectors.push("#js-poster-col > section.poster-list.-p230.-single.no-hover.el.col > div.production-statistic-list > div.production-statistic.-watches")
+            }
+            if (data.hideListAppears) {
+                hideSelectors.push("#js-poster-col > section.poster-list.-p230.-single.no-hover.el.col > div.production-statistic-list > div.production-statistic.-lists")
+            }
+            if (data.hideLikes) {
+                hideSelectors.push("#js-poster-col > section.poster-list.-p230.-single.no-hover.el.col > div.production-statistic-list > div.production-statistic.-likes")
+            }
+            if (data.hideTop250) {
+                hideSelectors.push("#js-poster-col > section.poster-list.-p230.-single.no-hover.el.col > div.production-statistic-list > div.production-statistic.-top250")
+            }
+
+            if (data.hideFriendsRatings) {
+                hideSelectors.push("#film-page-wrapper > div.col-17 > section.section.activity-from-friends.-clear.-friends-watched.-no-friends-want-to-watch")
+            }
+            if (data.hideFriendsReviews) {
+                hideSelectors.push("#film-page-wrapper > div.col-17 > section.film-recent-reviews.-clear > section.film-reviews.section.js-popular-friend-reviews")
+            }
+            if (data.hidePopularReviews) {
+                hideSelectors.push("#film-page-wrapper > div.col-17 > section.film-recent-reviews.-clear > section.film-reviews.section.js-popular-reviews")
+            }
+            if (data.hideRecentReviews) {
+                hideSelectors.push("#film-page-wrapper > div.col-17 > section.film-recent-reviews.-clear > section.film-reviews.section.js-recent-reviews")
+            }
+
+            if (data.hideSimilarFilms) {
+                hideSelectors.push("#film-page-wrapper > div.col-17 > section.section.related-films.-clear")
+            }
+
+            if (data.hidePopularLists) {
+                hideSelectors.push("#production-popular-lists")
+            }
+            if (data.hideYourLists) {
+                document.querySelectorAll("#lists > h2 > a").forEach(el => {
+                    if (el.innerHTML == "Your Lists") {
+                        hideElements.push(el.parentElement.parentElement)
+                    }
+                })
+            }
+            if (data.hideListsYouLiked) {
+                document.querySelectorAll("#lists > h2 > a").forEach(el => {
+                    if (el.innerHTML == "Lists you liked") {
+                        hideElements.push(el.parentElement.parentElement)
+                    }
+                })
+            }
+
+            // hide/show rating
+            let buttonExist = data.generateShowHideButton
+            function hideRatings() {
+                if (buttonExist) {
+                    const li = document.createElement("li")
+                    document.querySelector("#userpanel > ul").appendChild(li)
+                    const toggleBtn = document.createElement("button")
+                    toggleBtn.className = "toggleBtn"
+                    li.appendChild(toggleBtn)
+                }
+                function changeState(hidden) {
+                    let displayState
+                    if (hidden) {
+                        displayState = "none"
+                        if (buttonExist) {
+                            document.querySelector(".toggleBtn").textContent = "Show rating"
+                        }
+                    } else if (!hidden) {
+                        displayState = ""
+                        if (buttonExist) {
+                            document.querySelector(".toggleBtn").textContent = "Hide rating"
+                        }
+                    }
+                    hideSelectors.forEach(sel => { 
+                        const el = document.querySelector(sel)
+                        if (sel == "#film-page-wrapper > div.col-17 > section.section.activity-from-friends.-clear.-friends-watched.-no-friends-want-to-watch") {
+                            let friends = document.querySelectorAll("#film-page-wrapper > div.col-17 > section.section.activity-from-friends.-clear > ul > li > a > span.rating")
+                            friends.forEach(friend => {
+                                if (displayState) {
+                                    friend.style.display = "none"
+                                    let hiddenRating = document.createElement("span")
+                                    hiddenRating.textContent = "???"
+                                    hiddenRating.className = "hiddenRating"
+                                    hiddenRating.style.top = "4px"
+                                    hiddenRating.style.fontWeight = "600"
+                                    hiddenRating.style.position = "relative"
+                                    friend.parentElement.style.textAlign = "center"
+                                    friend.parentElement.appendChild(hiddenRating)
+                                } else if (!displayState) {
+                                    friend.style.display = ""
+                                    let deleteSpan = document.querySelector(".hiddenRating")
+                                    if (deleteSpan) {
+                                        deleteSpan.remove()
+                                    }
+                                }
+                            })
+                        } else {
+                            if (el) {
+                                el.style.display = displayState
                             }
                         }
                     })
-                } else {
-                    if (el) {
+                    hideElements.forEach(el => {
                         el.style.display = displayState
+                    })
+                }
+
+                function checkWatched() {
+                    function isWatched(selector) {
+                        const el = document.querySelector(selector)
+                        return el ? el.classList.contains('-on') : false
+                    }
+                    const checkSelectors = [
+                        "#userpanel > ul > li.actions-row1 > span.action-large.-watch > span > span > span",
+                        "#userpanel > ul > li.actions-row1 > span.action-large.-watch > a"
+                    ]
+                    let watched = checkSelectors.some(isWatched)
+                    const ratingEl = document.querySelector("#rateit-range-2")
+                    if (ratingEl && ratingEl.getAttribute("aria-valuenow") !== "0") {
+                        watched = true
+                    }
+
+                    if (watched) {
+                        hiddenState = false
+                        changeState(false)
+                    } else {
+                        hiddenState = true
+                        changeState(true)
                     }
                 }
-            })
+
+                let hiddenState = true
+                if (buttonExist) {
+                    document.querySelector(".toggleBtn").addEventListener('click', () => {
+                        if (hiddenState) {
+                            hiddenState = false
+                            changeState(hiddenState)
+                        } else if (!hiddenState) {
+                            hiddenState = true
+                            changeState(hiddenState)
+                        }
+                    })
+                }
+
+                checkWatched()
+            }
+            setTimeout(() => {
+                hideRatings()
+            }, 1000)
+
+            // initial run
+            setTimeout(() => {
+                loading = false
+                loadingSelectors.forEach(sel => { 
+                    const el = document.querySelector(sel)
+                    if (el) {
+                        el.style.visibility = "visible"
+                    }
+                })
+            }, 1000)
         }
-
-        function checkWatched() {
-            function isWatched(selector) {
-                const el = document.querySelector(selector)
-                return el ? el.classList.contains('-on') : false
-            }
-            const checkSelectors = [
-                "#userpanel > ul > li.actions-row1 > span.action-large.-watch > span > span > span",
-                "#userpanel > ul > li.actions-row1 > span.action-large.-watch > a"
-            ]
-            let watched = checkSelectors.some(isWatched)
-            const ratingEl = document.querySelector("#rateit-range-2")
-            if (ratingEl && ratingEl.getAttribute("aria-valuenow") !== "0") {
-                watched = true
-            }
-
-            if (watched) {
-                hiddenState = false
-                changeState(false)
-            } else {
-                hiddenState = true
-                changeState(true)
-            }
-        }
-
-        let hiddenState = true
-        toggleBtn.addEventListener('click', () => {
-            if (hiddenState) {
-                hiddenState = false
-                changeState(hiddenState)
-            } else if (!hiddenState) {
-                hiddenState = true
-                changeState(hiddenState)
-            }
-        })
-
-        checkWatched()
-    }
-
-    // initial run
-    setTimeout(() => {
-        loading = false
-        loadingSelectors.forEach(sel => { 
-            const el = document.querySelector(sel)
-            if (el) {
-                el.style.visibility = "visible"
-            }
-        })
-
-        hideRatings()
-    }, 1000)
+    })
 }
--- passive.js
+++ passive.js
@@ -1,76 +1,88 @@
-const selectorsToRemove = [
-    "#film-page-wrapper > div.col-17 > section.section-margin.film-news",
-    "#film-page-wrapper > div.col-17 > section.section.related-films.-clear > div.nanocrowd-attribution.-is-not-stacked",
-    "#film-hq-mentions",
-    "#content > div > div > aside > section.activity-settings.js-activity-filters.pro-message > form > small",
-]
- 
-// remove ads
-const ads = Array.from(document.querySelectorAll('div.banner.banner-950.js-hide-in-app, div.banner.banner-250.js-hide-in-app, div.banner.banner-230.js-hide-in-app')).filter(ad =>
-    ad.querySelector('a[href="/pro/?utm_medium=banner&utm_campaign=get-pro"]')
-)
-ads.forEach(ad => ad.remove())
-document.querySelectorAll(".upgrade-kicker").forEach(kicker => kicker.remove())
+chrome.storage.local.get(null, (data) => {
+    if (data.extensionState) {
+        const selectorsToRemove = []
+        // ===== GENERAL =====
+        if (data.adblock) {
+            // adblock
+            const ads = Array.from(document.querySelectorAll('div.banner.banner-950.js-hide-in-app, div.banner.banner-250.js-hide-in-app, div.banner.banner-230.js-hide-in-app')).filter(ad =>
+                ad.querySelector('a[href="/pro/?utm_medium=banner&utm_campaign=get-pro"]')
+            )
+            ads.forEach(ad => ad.remove())
+            document.querySelectorAll(".upgrade-kicker").forEach(kicker => kicker.remove())
 
-// function that run after passiveCooldown ms
-const passiveCooldown = 500
-function passive() {
-    // remove patron ad on film page
-    const filmPagePatronAd = document.querySelector("#userpanel > ul > li:nth-child(6) > a > span").parentElement.parentElement
-    if (filmPagePatronAd) filmPagePatronAd.remove()
+            // remove patron ad on film page
+            const filmPagePatronAdObserver = new MutationObserver((_, observer) => {
+                const link = Array.from(document.querySelectorAll("#userpanel a")).find(a => a.href.includes("/pro/"))
+                if (link) {
+                    link.closest("li").remove()
+                    observer.disconnect()
+                }
+            })
+            filmPagePatronAdObserver.observe(document.body, {childList: true, subtree: true})
 
-    // remove selectors
-    selectorsToRemove.forEach(selector => {
-        let selectorHTML = document.querySelector(selector)
-        if (selectorHTML) selectorHTML.remove()
-    })
-
-    // make reviews & network page wider
-    if (window.location.href.endsWith("/reviews/") || window.location.href.endsWith("/following/")) {
-        const reviews = document.querySelector("#content > div > section.section.col-main.overflow.col-17")
-        if (reviews) reviews.style.width = "100%"
-        const network = document.querySelector("#content > div > div > section")
-        if (network) network.style.width = "100%"
-    }
-
-    // activity settings
-    const activitySettingsForm = document.querySelector("#content > div > div > aside > section.activity-settings.js-activity-filters.pro-message > form")
-    if (activitySettingsForm) activitySettingsForm.style.paddingBottom = 0
-    const activitySettingsBtn1 = document.querySelector("#content > div > div > aside > section.activity-settings.js-activity-filters.pro-message > form > label:nth-child(1)")
-    if (activitySettingsBtn1) activitySettingsBtn1.style.paddingTop = "12px"
-    const activitySettingsBtn2 = document.querySelector("#content > div > div > aside > section.activity-settings.js-activity-filters.pro-message > form > label.option-label.-toggle.-small.switch-control.-block.-highcontrast.divider")
-    if (activitySettingsBtn2) activitySettingsBtn2.style.borderBottom = "none"
-    
-    // move rental slightly lower
-    const observer2 = new MutationObserver((mutations, obs) => {
-        const el = document.querySelector("#user-homepage-container > div.videostore-feature.section")
-        if (el) {
-            const next = el.nextElementSibling
-            if (next) next.after(el)
-            document.querySelector(".pw-div -nomargin -bottommargin").remove()
-            obs.disconnect()
+            // activity settings ad remove & padding consistency
+            selectorsToRemove.push("#content > div > div > aside > section.activity-settings.js-activity-filters.pro-message > form > small")
+            const activitySettingsForm = document.querySelector("#content > div > div > aside > section.activity-settings.js-activity-filters.pro-message > form")
+            if (activitySettingsForm) activitySettingsForm.style.paddingBottom = 0
+            const activitySettingsBtn1 = document.querySelector("#content > div > div > aside > section.activity-settings.js-activity-filters.pro-message > form > label:nth-child(1)")
+            if (activitySettingsBtn1) activitySettingsBtn1.style.paddingTop = "12px"
+            const activitySettingsBtn2 = document.querySelector("#content > div > div > aside > section.activity-settings.js-activity-filters.pro-message > form > label.option-label.-toggle.-small.switch-control.-block.-highcontrast.divider")
+            if (activitySettingsBtn2) activitySettingsBtn2.style.borderBottom = "none"
         }
-    })
-    observer2.observe(document.body, {childList: true, subtree: true})
 
-    // where to watch – remove the parent if not streaming
-    const justWatchMessage = document.querySelector("#watch > div.other.-message")
-    if (justWatchMessage) {
-        if (justWatchMessage.innerHTML.trim() == "Not streaming.") {
-            if (window.location.href.startsWith("https://letterboxd.com/film/")) {
-                document.querySelector("#js-poster-col > section.watch-panel.js-watch-panel > div.header").remove()
-                document.querySelector("#js-poster-col > section.watch-panel.js-watch-panel").remove()
-            } else {
-                document.querySelector("#content > div > div > section > div.col-4.gutter-right-1 > section.watch-panel.js-watch-panel").remove()
+        // ===== FILM PAGE =====
+        if (data.hideJustWatch) {
+            // where to watch – remove the parent if not streaming
+            const justWatchObserver = new MutationObserver((_, observer) => {
+                const justWatchMessage = document.querySelector("#watch > div.other.-message")
+                if (justWatchMessage) {
+                    if (justWatchMessage.innerHTML.trim() == "Not streaming.") {
+                        if (window.location.href.startsWith("https://letterboxd.com/film/")) {
+                            document.querySelector("#js-poster-col > section.watch-panel.js-watch-panel > div.header").remove()
+                            document.querySelector("#js-poster-col > section.watch-panel.js-watch-panel").remove()
+                        } else {
+                            document.querySelector("#content > div > div > section > div.col-4.gutter-right-1 > section.watch-panel.js-watch-panel").remove()
+                        }
+                    } else document.querySelector("#watch > div.other.-message").remove()
+                }
+            })
+            justWatchObserver.observe(document.body, {childList: true, subtree: true})
+        }
+
+        if (data.hideNews) {
+            selectorsToRemove.push("#film-page-wrapper > div.col-17 > section.section-margin.film-news")
+        }
+
+        if (data.hideMentionedBy) {
+            selectorsToRemove.push("#film-hq-mentions")
+        }
+        
+        // make reviews & network page wider
+        if ((window.location.href.endsWith("/reviews/") && !window.location.href.includes("film")) || window.location.href.endsWith("/following/")) {
+            if (data.reviewsPageWider) {
+                const reviews = document.querySelector("#content > div > section.section.col-main.overflow.col-17")
+                if (reviews) reviews.style.width = "100%"
             }
-        } else document.querySelector("#watch > div.other.-message").remove()
+            if (data.networkPageWider) {
+                const network = document.querySelector("#content > div > div > section")
+                if (network) network.style.width = "100%"
+            }
+        }
+
+        // people pages consistent border-radius
+        peopleShare = document.querySelector("#userpanel > ul > li")
+        if (peopleShare) peopleShare.style.borderRadius = "3px"
+
+        // remove selectors
+        selectorsToRemove.forEach(selector => {
+            const observer = new MutationObserver((_, observer) => {
+                const selectorHTML = document.querySelector(selector)
+                if (selectorHTML) {
+                    selectorHTML.remove()
+                    observer.disconnect()
+                }
+            })
+            observer.observe(document.body, {childList: true, subtree: true})
+        })
     }
-
-    // people pages share border-radius
-    peopleShare = document.querySelector("#userpanel > ul > li")
-    if (peopleShare) peopleShare.style.borderRadius = "3px"
-}
-
-setTimeout(() => {
-    passive()
-}, passiveCooldown)
+})
